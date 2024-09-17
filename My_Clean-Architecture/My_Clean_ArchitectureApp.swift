5465676c3c6485b2ac61739157aa119eb7882cc5
carki <94.nogari@gmail.com>
2024-09-18 03:43:09 +0900

    Initial Commit

---
+++ My_Clean-Architecture/My_Clean_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Clean_ArchitectureApp.swift
+//  My_Clean-Architecture
+//
+//  Created by Carki on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct My_Clean_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
